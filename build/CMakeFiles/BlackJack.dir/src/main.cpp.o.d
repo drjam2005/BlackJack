0f91a49307a27ab01fd869e2e75a509e699b76d3
drjam2005 <jamesallanmdr2005@gmail.com>
2026-06-13 20:40:08 +0800

    chore: removed ignored files

--- build/CMakeFiles/BlackJack.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/BlackJack.dir/src/main.cpp.o: \
- /home/james/Projects/BlackJack/src/main.cpp /usr/include/stdc-predef.h \
- /home/james/Projects/BlackJack/vendor/raylib/unix/include/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h
